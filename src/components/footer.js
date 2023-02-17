3cdb4a9cc10f3e2cb79f0535aab8e5e8f45ef952
ys-sudo <georgios.lazaridis0@gmail.com>
2023-02-17 08:59:35 +0100

    make footer responsive with kofi

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,16 +14,15 @@ const about = () => {
 const Footer = () => {
   return(
   <>
-  <div style={{display:'flex',position:'fixed',bottom:'15px',width:'100%',justifyContent:'space-evenly',alignItems:'center',flexWrap:'wrap'}}>
-    <Button variant="outlined" color="secondary" onClick={earlyAccess}>
+  <div style={{display:'flex',position:'fixed',bottom:'15px',maxWidth:'50%',marginLeft:'50%',justifyContent:'space-evenly',alignItems:'right',flexWrap:'wrap'}}>
+    <Button style={{margin:'10px'}} variant="outlined" color="secondary" onClick={earlyAccess}>
     Early Access
     </Button>
-    <Button variant="outlined" color="secondary" onClick={about}>
+    <Button style={{margin:'10px'}} variant="outlined" color="secondary" onClick={about}>
     About
     </Button>
-    <a href="https://github.com/ys-sudo/gatsby-openai-firebase-starter" title="Go to GitHub repo"><img src="https://img.shields.io/static/v1?label=ys-sudo&message=gatsby-openai-firebase-starter&color=9c27b0&logo=github" alt="ys-sudo - gatsby-openai-firebase-starter" /></a>
-    <a href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/stars/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="stars - gatsby-openai-firebase-starter" /></a>
-    <a href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/forks/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="forks - gatsby-openai-firebase-starter" /></a>
+    <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/stars/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="stars - gatsby-openai-firebase-starter" /></a>
+    <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/forks/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="forks - gatsby-openai-firebase-starter" /></a>
   </div>
   </>
 )
