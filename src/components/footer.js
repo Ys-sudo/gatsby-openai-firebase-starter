8cd6e1cb7c3936abd66fa1f5e8ca3a7ac8aa6215
ys-sudo <georgios.lazaridis0@gmail.com>
2023-02-17 18:41:32 +0100

    udpate footer nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,15 +14,18 @@ const about = () => {
 const Footer = () => {
   return(
   <>
-  <div style={{display:'flex',position:'fixed',bottom:'15px',maxWidth:'50%',marginLeft:'50%',justifyContent:'space-evenly',alignItems:'right',flexWrap:'wrap'}}>
+  <div style={{display:'flex',position:'fixed',bottom:'15px',right:'10px',maxWidth:'60%',marginLeft:'40%',justifyContent:'space-evenly',alignItems:'center',flexWrap:'wrap'}}>
     <Button style={{margin:'10px'}} variant="outlined" color="secondary" onClick={earlyAccess}>
     Early Access
     </Button>
     <Button style={{margin:'10px'}} variant="outlined" color="secondary" onClick={about}>
     About
     </Button>
-    <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/stars/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="stars - gatsby-openai-firebase-starter" /></a>
-    <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/forks/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="forks - gatsby-openai-firebase-starter" /></a>
+  </div>
+  <div style={{position:'fixed',left:'10px',bottom:'75px'}}>
+  <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/stars/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="stars - gatsby-openai-firebase-starter" /></a>
+  <br />
+  <a style={{margin:'10px'}} href="https://github.com/ys-sudo/gatsby-openai-firebase-starter"><img src="https://img.shields.io/github/forks/ys-sudo/gatsby-openai-firebase-starter?style=social" alt="forks - gatsby-openai-firebase-starter" /></a>
   </div>
   </>
 )
