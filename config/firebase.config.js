d89fb51deddab479b867e6a07707ffa16d6b285e
“Ys-sudo” <georgios.lazaridis0@gmail.com>
2024-06-23 02:30:42 +0200

    test fix for build

--- config/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
--- config/firebase.js
+++ config/firebase.js
@@ -5,8 +5,19 @@ import 'firebase/compat/analytics'
 import 'firebase/compat/database'
 import 'firebase/auth'
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+require('dotenv').config();
 
-export const Firebase = initializeApp(firebaseConfig);
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  };
+
+export const Firebase = initializeApp(config);
 export const auth = getAuth();
 export const Providers = { google: new GoogleAuthProvider() };
 
